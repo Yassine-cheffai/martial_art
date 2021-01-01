1697b88c001cda685524ff71ba0e04b885ab8b5d
Rjab Chaalia <rejeb.chaalia.com>
2021-01-01 23:17:24 +0100

    add home page

--- martial/urls.py
+++ martial/urls.py
@@ -24,11 +24,13 @@ from django.views.static import serve
 from profiles.views import MyHomeView
 
 urlpatterns = [
-    path('', MyHomeView.as_view(), name='index'),
+    # path('', MyHomeView.as_view(), name='index'),
+    path('', include('team.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('profiles/', include('profiles.urls')),
+
 ]
 
 
