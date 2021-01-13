51c94803986938d16b35f71d81d16e3a899329b1
khaled_ha <hadjalikhaled@gmail.com>
2021-01-13 22:01:59 +0100

    clean code : delete group app , post and comment

--- groups/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-
-from groups import views
-
-path('groups/<int:user_id>/home/', views.GroupHomeView),
-path('groups/<int:group_id>/detail/', views.GroupDetailView),
--- profiles/admin.py
+++ profiles/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
 
-from profiles.models import Post
-
-
 # Register your models here.
-class PostAdmin(admin.ModelAdmin):
-    exclude = ('created_time', 'last_modified')
-    list_display = ('owner', 'created_time', 'last_modified', 'content')
-
-
-admin.site.register(Post, PostAdmin)
