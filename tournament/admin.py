69905bff1ed48e85d9c4a2cd6d8483317de89ad3
Yassine-cheffai <yacincheffai@yahoo.com>
2021-01-16 21:44:31 +0100

    add competition to admin

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Tournament, Competition
 
-# Register your models here.
+
+class CompetitionAdmin(admin.TabularInline):
+    model = Competition
+
+
+@admin.register(Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    inlines = [CompetitionAdmin]
+
+
+admin.site.register(Competition)
