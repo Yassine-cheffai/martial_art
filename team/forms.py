f657cf9f37838f4f6a783dc72613c1e61e4c76d2
Yassine-cheffai <yacincheffai@yahoo.com>
2021-02-14 17:18:36 +0100

    fix

--- team/forms.py
+++ team/forms.py
@@ -10,6 +10,13 @@ class ParticipationForm(forms.ModelForm):
     def clean(self):
         competitors = self.cleaned_data.get("competitors")
         competition = self.cleaned_data.get("competition")
+
+        if not competitors:
+            raise ValidationError("you should select competitors")
+
         for competitor in competitors:
             if competitor.weight > competition.competitors_weight:
                 raise ValidationError("competitor weight surpass the competition max weight")
+
+        self._validate_unique = True
+        return self.cleaned_data
